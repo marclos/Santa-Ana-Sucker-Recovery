bdd245a1055f838ff7375f71edcce60c25eed6c4
Nicole Larson <nllk2015@mymail.pomona.edu>
2016-10-12 15:41:57 -0700

    added

--- Data/Data_Weds_1/DateTime_Factor_Convert.R
+++ Data/Data_Weds_1/DateTime_Factor_Convert.R
@@ -7,16 +7,20 @@ site1 <- read.csv("/home/CAMPUS/mwl04747/github/Santa-Ana-Sucker-Recovery/Data/D
 
 str(site1)
 
+site1$TempCor<-site1$Temp-.121
+site2$TempCor<-site2$Temp-.121
+site3$TempCor<-site3$Temp-.232
+site4$TempCor<-site4$Temp-.232
+
 # Assuming PDT!
 site1$datetime <- as.POSIXct(site1$DateTime,format="%m/%d/%Y %H:%M")
 site2$datetime <- as.POSIXct(site2$DateTime,format="%m/%d/%Y %H:%M")
 site3$datetime <- as.POSIXct(site3$DateTime,format="%m/%d/%Y %H:%M")
 site4$datetime <- as.POSIXct(site4$DateTime,format="%m/%d/%Y %H:%M")
 
-plot(site4$datetime, site4$Temp, ty='l')
-lines(site3$datetime, site3$Temp, col='green')
-lines(site2$datetime, site2$Temp, col='red')
-
-lines(site1$datetime, site1$Temp, col='blue')
+plot(site3$datetime, site3$TempCor, ty='l', col='green', ylim=c(25,32), las=1)
+lines(site4$datetime, site4$TempCor, col='black')
+lines(site2$datetime, site2$TempCor, col='red')
+lines(site1$datetime, site1$TempCor, col='blue')
 
 
