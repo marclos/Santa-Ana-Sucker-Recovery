bb95dff4eb1ee216f7e8577352b9e00cca1bf41e
marclos <marc.loshuertos@pomona.edu>
2016-10-15 13:28:58 -0700

    figure for report

--- Data/Data_Weds_1/DateTime_Factor_Convert.R
+++ Data/Data_Weds_1/DateTime_Factor_Convert.R
@@ -18,18 +18,26 @@ site2$datetime <- as.POSIXct(site2$DateTime,format="%m/%d/%Y %H:%M")
 site3$datetime <- as.POSIXct(site3$DateTime,format="%m/%d/%Y %H:%M")
 site4$datetime <- as.POSIXct(site4$DateTime,format="%m/%d/%Y %H:%M")
 
-<<<<<<< HEAD
 # Creates a plot of time using lines
 plot(site4$datetime, site4$Temp, ty='l', 
-     ylab="Temperature (C)", xlab="Date", las=1, main="Temperature for Site 4", ylim=c(25, 32))
+     ylab="Temperature (C)", xlab="Date", las=1, main="Temperature Readings", ylim=c(25, 32))
 lines(site3$datetime, site3$Temp, col='darkgreen')
 lines(site2$datetime, site2$Temp, col='red')
 lines(site1$datetime, site1$Temp, col='blue')
-=======
+
 plot(site3$datetime, site3$TempCor, ty='l', col='green', ylim=c(25,32), las=1)
 lines(site4$datetime, site4$TempCor, col='black')
 lines(site2$datetime, site2$TempCor, col='red')
 lines(site1$datetime, site1$TempCor, col='blue')
->>>>>>> 3fd2c51474665f500f6710eb6cfde473ac22b89e
 
+
+# Creating a Figure as a pdf!
+
+pdf(file="Final_Report/Figures/Temp.pdf")
+plot(site4$datetime, site4$Temp, ty='l', 
+     ylab="Temperature (C)", xlab="Date", las=1, main="Time Series of Temperature Data", ylim=c(25, 32))
+lines(site3$datetime, site3$Temp, col='darkgreen')
+lines(site2$datetime, site2$Temp, col='red')
+lines(site1$datetime, site1$Temp, col='blue')
+dev.off()
 
